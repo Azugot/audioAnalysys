e91247a9a2ca210289d6fdb0c164c0de1cf956d3
AugustoSZer0 <augustoscardua@gmail.com>
2023-05-04 14:14:47 -0300

    Rest of the code

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ import tensorflow as tf
 import tensorflow_io as tfio
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, Dense, Flatten
+from tensorflow.keras.preprocessing import timeseries_dataset_from_array
+from itertools import groupby
+import csv
+#Functions=========================
 
-# Path to the audio files
-CAPUCHIN_FILE = os.path.join(
-    'data', 'Parsed_Capuchinbird_Clips', 'XC3776-3.wav')
-NOT_CAPUCHIN_FILE = os.path.join(
-    'data', 'Parsed_Not_Capuchinbird_Clips', 'afternoon-birds-song-in-forest-0.wav')
-
-
+#Load WAVs
 def load_wav_16k_mono(filename):
     # Load encoded wav file
     file_contents = tf.io.read_file(filename)
@@ -25,6 +23,21 @@ def load_wav_16k_mono(filename):
     wav = tfio.audio.resample(wav, rate_in=sample_rate, rate_out=16000)
     return wav
 
+#Load up MP3s
+def load_mp3_16k_mono(filename):
+    """ Load a WAV file, convert it to a float tensor, resample to 16 kHz single-channel audio. """
+    res = tfio.audio.AudioIOTensor(filename)
+    # Convert to tensor and combine channels 
+    tensor = res.to_tensor()
+    tensor = tf.math.reduce_sum(tensor, axis=1) / 2 
+    # Extract sample rate and cast
+    sample_rate = res.rate
+    sample_rate = tf.cast(sample_rate, dtype=tf.int64)
+    # Resample to 16 kHz
+    wav = tfio.audio.resample(tensor, rate_in=sample_rate, rate_out=16000)
+    return wav
+
+    #Preprocess Data
 def preprocess(file_path, label): 
     wav = load_wav_16k_mono(file_path)
     wav = wav[:48000]
@@ -35,23 +48,25 @@ def preprocess(file_path, label):
     spectrogram = tf.expand_dims(spectrogram, axis=2)
     return spectrogram, label
 
+#Build Function to Convert Clips into Windowed Spectrograms
+def preprocess_mp3(sample, index):
+    sample = sample[0]
+    zero_padding = tf.zeros([48000] - tf.shape(sample), dtype=tf.float32)
+    wav = tf.concat([zero_padding, sample],0)
+    spectrogram = tf.signal.stft(wav, frame_length=320, frame_step=32)
+    spectrogram = tf.abs(spectrogram)
+    spectrogram = tf.expand_dims(spectrogram, axis=2)
+    return spectrogram
 
-wave = load_wav_16k_mono(CAPUCHIN_FILE) #wave function of the capuchin bird
-nwave = load_wav_16k_mono(NOT_CAPUCHIN_FILE)#wave function withou the capuchin bird
-
-#ploting the waves using matplotlib
-"""plt.plot(wave) 
-plt.plot(nwave)
-plt.show()
-"""
-
+#==================================
 #Reading the files from the folders
 POS = os.path.join('data', 'Parsed_Capuchinbird_Clips')
 NEG = os.path.join('data', 'Parsed_Not_Capuchinbird_Clips')
+mp3 = os.path.join('data', 'Forest Recordings', 'recording_00.mp3')
 
 #Creating the datasets
-pos = tf.data.Dataset.list_files(POS, '\*.wav')
-neg = tf.data.Dataset.list_files(NEG, '\*.wav')
+pos = tf.data.Dataset.list_files(POS+'\*.wav')
+neg = tf.data.Dataset.list_files(NEG+'\*.wav')
 
 #Adding the labels to the datasets
 positives = tf.data.Dataset.zip((pos, tf.data.Dataset.from_tensor_slices(tf.ones(len(pos)))))
@@ -59,9 +74,6 @@ negatives = tf.data.Dataset.zip((neg, tf.data.Dataset.from_tensor_slices(tf.zero
 
 #Concatenating the datasets
 data = positives.concatenate(negatives)
-
-#Shuffling the data
-data = data.shuffle(buffer_size=1024)
 
 #Calculate the wave length cycle
 lenghts = []
@@ -74,10 +86,105 @@ tf.math.reduce_min(lenghts)
 tf.math.reduce_mean(lenghts)
 tf.math.reduce_max(lenghts)
 
-filepath, label = positives.shuffle(buffer_size=10000).as_numpy_iterator().next()
+#Creating the Data Pipelines
+data = data.map(preprocess)
+data = data.cache()
+data = data.shuffle(buffer_size=1000)
+data = data.batch(16)
+data = data.prefetch(8)
 
-spectrogram = preprocess(filepath, label)
+#Splitting the data into train and test partitions
+train = data.take(36)
+test = data.skip(36).take(15)
 
-plt.figure(figsize=(30,20))
-plt.imshow(tf.transpose(spectrogram)[0])
+#Building the model
+model = Sequential()
+model.add(Conv2D(12, (3,3), activation='relu', input_shape=(1491, 257,1)))
+model.add(Conv2D(12, (3,3), activation='relu'))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+#Compiling the model
+model.compile('Adam', loss='BinaryCrossentropy', metrics=[tf.keras.metrics.Recall(),tf.keras.metrics.Precision()])
+
+#Summary of the model
+model.summary()
+
+hist = model.fit(train, epochs=4, validation_data=test)
+"""
+#FFit Model, View Loss and KPI Plots
+plt.title('Loss')
+plt.plot(hist.history['loss'], 'r')
+plt.plot(hist.history['val_loss'], 'b')
 plt.show()
+
+plt.title('Precision')
+plt.plot(hist.history['precision'], 'r')
+plt.plot(hist.history['val_precision'], 'b')
+plt.show()
+
+plt.title('Recall')
+plt.plot(hist.history['recall'], 'r')
+plt.plot(hist.history['val_recall'], 'b')
+plt.show()
+"""
+#predict one batch of test data
+X_test, y_test = test.as_numpy_iterator().next()
+
+yhat = model.predict(X_test)
+
+#Convert Logits to Classes
+yhat = [1 if prediction > 0.5 else 0 for prediction in yhat]
+
+#Loads the MP3 file and converts it to a WAV
+wav = load_mp3_16k_mono(mp3)
+
+audio_slices = tf.keras.preprocessing.timeseries_dataset_from_array(wav, wav, sequence_length=48000, sequence_stride=48000, batch_size=1)
+samples, index = audio_slices.as_numpy_iterator().next()
+
+#Convert Longer Clips into Windows and Make Predictions
+audio_slices = tf.keras.preprocessing.timeseries_dataset_from_array(wav, wav, sequence_length=16000, sequence_stride=16000, batch_size=1)
+audio_slices = audio_slices.map(preprocess_mp3)
+audio_slices = audio_slices.batch(64)
+
+yhat = model.predict(audio_slices)
+yhat = [1 if prediction > 0.5 else 0 for prediction in yhat]
+
+#Group Consecutive Detections
+yhat = [key for key, group in groupby(yhat)]
+calls = tf.math.reduce_sum(yhat).numpy()
+
+calls
+
+#Loop over all recordings and make predictions
+results = {}
+for file in os.listdir(os.path.join('data', 'Forest Recordings')):
+    FILEPATH = os.path.join('data','Forest Recordings', file)
+    
+    wav = load_mp3_16k_mono(FILEPATH)
+    audio_slices = tf.keras.preprocessing.timeseries_dataset_from_array(wav, wav, sequence_length=48000, sequence_stride=48000, batch_size=1)
+    audio_slices = audio_slices.map(preprocess_mp3)
+    audio_slices = audio_slices.batch(64)
+    
+    yhat = model.predict(audio_slices)
+    
+    results[file] = yhat
+
+results
+
+class_preds = {}
+for file, logits in results.items():
+    class_preds[file] = [1 if prediction > 0.99 else 0 for prediction in logits]
+class_preds
+
+postprocessed = {}
+for file, scores in class_preds.items():
+    postprocessed[file] = tf.math.reduce_sum([key for key, group in groupby(scores)]).numpy()
+postprocessed
+
+with open('results.csv', 'w', newline='') as f:
+    writer = csv.writer(f, delimiter=',')
+    writer.writerow(['recording', 'capuchin_calls'])
+    for key, value in postprocessed.items():
+        writer.writerow([key, value])
